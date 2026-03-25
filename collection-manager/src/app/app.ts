4ddb4b9bc8872488651285b4abbb7a27b0df2319
Berin Drustinac <Berin358@outlook.fr>
2026-03-25 10:02:51 +0100

    component

--- collection-manager/src/app/app.ts
+++ collection-manager/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CollectionItemCard } from './components/collection-item-card/collection-item-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CollectionItemCard],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('collection-manager');
---
+++ collection-manager/src/app/components/collection-item-card/collection-item-card.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collection-item-card',
+  imports: [],
+  templateUrl: './collection-item-card.html',
+})
+export class CollectionItemCard { }
