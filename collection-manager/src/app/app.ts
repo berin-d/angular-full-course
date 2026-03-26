323ce7b8aa4ccac8df62d191c49b05bc3d454536
Berin Drustinac <Berin358@outlook.fr>
2026-03-26 09:51:54 +0100

    output

--- collection-manager/src/app/app.ts
+++ collection-manager/src/app/app.ts
@@ -1,13 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { CollectionItemCard } from './components/collection-item-card/collection-item-card';
 import { CollectionItem } from './models/collection-item';
+import { SearchBar } from "./components/search-bar/search-bar";
+
 
 @Component({
   selector: 'app-root',
-  imports: [CollectionItemCard],
+  imports: [CollectionItemCard, SearchBar],
   templateUrl: './app.html',
 })
 export class App {
+
+  count = 0;
+  searchText = "";
+
   coin: CollectionItem;
 
   constructor() {
@@ -17,5 +23,10 @@ export class App {
     this.coin.rarity = "Rare";
     this.coin.image = "img/pngegg-pink.png";
     this.coin.price = 149;
+  }
+
+
+  incrementCount() {
+    this.count++;
   }
 }
---
+++ collection-manager/src/app/components/search-bar/search-bar.ts
@@ -0,0 +1,28 @@
+import { Component, output, OutputEmitterRef, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+// Models: la fonction model est un input et un output à la fois (two-way binding )
+
+@Component({
+  selector: 'app-search-bar',
+  imports: [FormsModule],
+  templateUrl: './search-bar.html',
+})
+export class SearchBar {
+
+  // Inputs
+  search = model("Initial Search");
+
+  // Outputs
+  searchButtonClicked: OutputEmitterRef<void> = output<void>({
+    alias: "submit",
+  });
+
+
+
+
+  searchClick() {
+    this.searchButtonClicked.emit();
+  }
+
+}
